f8897ec69e5e7b515126026a7333033f3d3ff675
Anna Lieb <anna@kepsi.eu>
2021-07-23 11:24:26 -0400

    clean adding of stuff

---
+++ pathonoia/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from setuptools import setup
+
+setup(name='pathonoia',
+version='0.1',
+description='Samplewide species abundance based on Kraken2 output',
+author='Anna Liebhoff',
+author_email='aliebho1@jhu.edu',
+license='MIT',
+packages=['p_sample'],
+zip_safe=False)
